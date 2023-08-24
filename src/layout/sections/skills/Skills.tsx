f7b28b94457240c79cc21a0a4ba727076767354f
Oleg Rijikov <olegrij90@gmail.com>
2023-08-24 19:19:05 +0300

    skill component add to skills layout

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -3,13 +3,16 @@ import styled from "styled-components";
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Icon} from "../../../components/icon/Icon";
 import {SectionTitle} from "../../../components/SectionTitle";
+import {Skill} from "./skill/Skill";
 
 export const Skills = () => {
     return (
         <StyledSkills>
             <SectionTitle></SectionTitle>
             <FlexWrapper>
-
+                <Skill/>
+                <Skill/>
+                <Skill/>
             </FlexWrapper>
         </StyledSkills>
     );
